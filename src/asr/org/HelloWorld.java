fca0a83d49fccee3030d312be976621b08a88ca2
Gill <devops.cgill@gmail.com>
2021-06-06 18:05:36 +0000

    changed by cg

--- src/asr/org/HelloWorld.java
+++ src/asr/org/HelloWorld.java
@@ -7,11 +7,11 @@ public class HelloWorld {
 		
 		System.out.println("********************************I am at starts***********************************");
 		System.out.println("This change was made by singh brothers");
-		System.out.println("This change was made by Sarmail in Dev branch");
+		System.out.println("This change was made by gill in his Dev branch from AWS machine under gill id");
 		int i, z;
 		int y;
-		i = 112;
-		y = 134;
+		i = 100;
+		y = 100;
 		z = i + y;
 		System.out.println("**i = " + i);
 		System.out.println("***y = " + i);
