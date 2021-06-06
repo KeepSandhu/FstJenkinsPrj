418192fdd3bb425df133242436f6e274211088b4
Baani <keepsandhu@gmail.com>
2021-06-05 21:46:29 -0400

    first commit

---
+++ src/asr/org/HelloWorld.java
@@ -0,0 +1,19 @@
+package asr.org;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("********************************I am at starts***********************************");
+		int i, z;
+		int y;
+		i = 4;
+		y = 5;
+		z = i + y;
+		System.out.println("********************************I am at End ***********************************z = " + z);
+		
+
+	}
+
+}
